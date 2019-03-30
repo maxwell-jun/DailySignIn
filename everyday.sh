1d4421cf5f4d79d51d2c9f12b103f3cd210d74f8
Kherrisan <zdkscope@qq.com>
2019-03-30 08:54:43 -0400

    20190330-085443

--- everyday.sh
+++ everyday.sh
@@ -1,1 +1,1 @@
-echo `date +%Y%m%d-%H%M%S` >> /home/zdk/DailySignIn/daily && cd /home/zdk/DailySignIn && git add . && git commit -m `date +%Y%m%d-%H%M%S` && git push
+echo `date +%Y%m%d-%H%M%S` >> /root/DailySignIn/daily && cd /root/DailySignIn && git add . && git commit -m `date +%Y%m%d-%H%M%S` && git push

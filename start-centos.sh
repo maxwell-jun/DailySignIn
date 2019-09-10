31429ca84d93856c38256814cdab3c6c032ac304
maxwell <maxwell_jun@163.com>
2019-09-10 03:03:02 +0000

    20190910-030301

---
+++ start-centos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+basepath=$(cd `dirname $0`; pwd)
+if [ ! -e `which crontab` ];then
+    `yum install -y crontabs`
+fi
+echo '00 12 * * * echo `date +%Y%m%d-%H%M%S` >> '$basepath/daily' && cd '$basepath' && git add . && git commit -m `date +%Y%m%d-%H%M%S` && git push' >> /etc/crontab
+if [[ ! -e "$basepath/daily" ]];then
+    `touch $basepath/daily`
+fi
